cd62274ba0ccb900cf90dc079622c8b081c5f1a9
Khrystyna <khrystynavysotska@gmail.com>
2021-11-19 02:04:10 +0200

    Added new api endpoint to constants

--- lib/api/constants.dart
+++ lib/api/constants.dart
@@ -1,3 +1,4 @@
 const String BASE_URL = "https://cloud-lab-3-332215.uc.r.appspot.com";
 
 const String RIVER_ENDPOINT = "/river";
+const String LATEST_MEASUREMENTS_ENDPOINT = "/measurement_point/latest";
