a467aaa1c4d8ed05c1d73c315c8761c623dca60a
Khrystyna <khrystynavysotska@gmail.com>
2021-11-19 02:13:08 +0200

    Added latest measurement section that draws UI based on state

---
+++ lib/widgets/measurements/latest_measurements_section.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:river_water_level/bloc/blocs/latest_measurements_bloc.dart';
+import 'package:river_water_level/bloc/events/latest_measurements_event.dart';
+import 'package:river_water_level/bloc/states/latest_measurements_state.dart';
+import 'package:river_water_level/widgets/measurements/latest_measurements_carousel.dart';
+
+class LatestMeasurementsSection extends StatefulWidget {
+  const LatestMeasurementsSection({Key? key}) : super(key: key);
+
+  @override
+  _LatestMeasurementsSectionState createState() =>
+      _LatestMeasurementsSectionState();
+}
+
+class _LatestMeasurementsSectionState extends State<LatestMeasurementsSection> {
+  final LatestMeasurementsBloc _latestMeasurementsBloc =
+      LatestMeasurementsBloc();
+
+  @override
+  void initState() {
+    super.initState();
+    _latestMeasurementsBloc.inputEventSink.add(LatestMeasurementsLoadEvent());
+  }
+
+  @override
+  void dispose() {
+    _latestMeasurementsBloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: _latestMeasurementsBloc.outputStateStream,
+      initialData: _latestMeasurementsBloc.initialState,
+      builder: (context, snapshot) {
+        if (snapshot.data is LatestMeasurementsEmptyState) {
+          return const Center(child: Text("No data received"));
+        }
+        if (snapshot.data is LatestMeasurementsLoadingState) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.data is LatestMeasurementsLoadedState) {
+          return LatestMeasurementsCarousel(
+            latestMeasurements: (snapshot.data as LatestMeasurementsLoadedState)
+                .loadedLatestMeasurements,
+          );
+        }
+        if (snapshot.data is LatestMeasurementsFailedState) {
+          return const Center(
+            child: Text("Error fetching latest measurements"),
+          );
+        }
+        return const Text('Unknown state');
+      },
+    );
+  }
+}
