3ae467bcde8471d1968ce206179d95a4a905675a
Khrystyna <khrystynavysotska@gmail.com>
2021-11-19 02:05:30 +0200

    Modified rivers page to show sliver app bar with latest measurements

--- lib/pages/rivers_page.dart
+++ lib/pages/rivers_page.dart
@@ -1,18 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:river_water_level/models/river.dart';
+import 'package:river_water_level/widgets/common/page_header.dart';
+import 'package:river_water_level/widgets/measurements/latest_measurements_section.dart';
 import 'package:river_water_level/widgets/rivers/rivers_list.dart';
 
 class RiversPage extends StatelessWidget {
   const RiversPage({Key? key, required this.rivers}) : super(key: key);
 
+  final bool _snap = false;
+  final bool _pinned = false;
+  final bool _floating = true;
+
   final List<River> rivers;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Expanded(child: RiversList(rivers: rivers)),
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          snap: _snap,
+          pinned: _pinned,
+          floating: _floating,
+          expandedHeight: 160.0,
+          backgroundColor: Theme.of(context).primaryColor,
+          flexibleSpace: const FlexibleSpaceBar(
+            background: LatestMeasurementsSection(),
+          ),
+        ),
+        const SliverToBoxAdapter(child: PageHeader(header: 'Оберіть річку:')),
+        RiversList(rivers: rivers),
       ],
     );
   }
