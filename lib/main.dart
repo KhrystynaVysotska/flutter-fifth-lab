ee810759612c88abea101f3d57a9ce8c52d74fa0
Khrystyna <khrystynavysotska@gmail.com>
2021-11-19 02:07:18 +0200

    Wrapped app with provider, changed home page and added theme color

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:river_water_level/pages/rivers_page_wrapper.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:river_water_level/bloc/blocs/alert_measurements_bloc.dart';
+import 'package:river_water_level/pages/home_page.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(Provider(
+      create: (_) => AlertMeasurementsBloc(),
+      child: const MyApp(),
+    ));
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    const primaryColor = Color(0xfff0f7ff);
+
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: primaryColor,
+      statusBarBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark,
+    ));
+
+    return MaterialApp(
       title: "Rivers water level",
+      theme: ThemeData(primaryColor: primaryColor),
       debugShowCheckedModeBanner: false,
-      home: RiversPageWrapper(),
+      home: const HomePage(),
     );
   }
 }
