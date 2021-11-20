4d8413ce73eaf777cb57c3e725c7208f078f2cd2
Khrystyna <khrystynavysotska@gmail.com>
2021-11-20 22:17:19 +0200

    Customized page header with ability to add arrow back

--- lib/widgets/common/page_header.dart
+++ lib/widgets/common/page_header.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
 
 class PageHeader extends StatelessWidget {
-  const PageHeader({Key? key, required this.header}) : super(key: key);
+  const PageHeader({Key? key, required this.header, required this.hasArrowBack})
+      : super(key: key);
 
   final String header;
+  final bool hasArrowBack;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40.0),
-      child: Text(
-        header,
-        style: const TextStyle(
-          fontSize: 20,
-          color: Color(0xff3c404a),
-          fontWeight: FontWeight.w700,
-        ),
+      padding: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: hasArrowBack ? 20.0 : 40.0,
+      ),
+      child: Row(
+        children: [
+          if (hasArrowBack)
+            IconButton(
+              icon: const Icon(Icons.arrow_back, color: Colors.black),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          Text(
+            header,
+            style: const TextStyle(
+              fontSize: 20,
+              color: Color(0xff3c404a),
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ],
       ),
     );
   }
