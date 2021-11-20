9b2bd50eb42d81d6430d232db2f2515e939c78dc
Khrystyna <khrystynavysotska@gmail.com>
2021-11-20 22:24:46 +0200

    Moved app bar to separate component

--- lib/pages/rivers_page.dart
+++ lib/pages/rivers_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:river_water_level/controllers/latest_measurements_state_controller.dart';
 import 'package:river_water_level/models/river.dart';
 import 'package:river_water_level/widgets/common/page_header.dart';
 import 'package:river_water_level/widgets/rivers/rivers_list.dart';
@@ -7,27 +6,18 @@ import 'package:river_water_level/widgets/rivers/rivers_list.dart';
 class RiversPage extends StatelessWidget {
   const RiversPage({Key? key, required this.rivers}) : super(key: key);
 
-  final bool _snap = false;
-  final bool _pinned = false;
-  final bool _floating = true;
-
   final List<River> rivers;
 
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: <Widget>[
-        SliverAppBar(
-          snap: _snap,
-          pinned: _pinned,
-          floating: _floating,
-          expandedHeight: 160.0,
-          backgroundColor: Theme.of(context).primaryColor,
-          flexibleSpace: const FlexibleSpaceBar(
-            background: LatestMeasurementsStateController(),
+        const SliverToBoxAdapter(
+          child: PageHeader(
+            header: 'Оберіть річку:',
+            hasArrowBack: false,
           ),
         ),
-        const SliverToBoxAdapter(child: PageHeader(header: 'Оберіть річку:')),
         RiversList(rivers: rivers),
       ],
     );
