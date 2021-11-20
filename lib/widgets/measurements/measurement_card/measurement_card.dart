058e299ceca215d73c642e65fb765c19e6cc5c68
Khrystyna <khrystynavysotska@gmail.com>
2021-11-20 22:23:41 +0200

    Added onTap functionality and used global bloc

--- lib/widgets/measurements/measurement_card/measurement_card.dart
+++ lib/widgets/measurements/measurement_card/measurement_card.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:river_water_level/models/latest_measurement.dart';
+import 'package:river_water_level/bloc/blocs/redirection_to_measurements_bloc.dart';
+import 'package:river_water_level/bloc/events/redirection_to_measurements_event.dart';
 import 'package:river_water_level/widgets/measurements/measurement_card/measurement_card_location.dart';
 import 'package:river_water_level/widgets/measurements/measurement_card/measurement_card_timestamp.dart';
 import 'package:river_water_level/widgets/measurements/measurement_card/measurement_card_value.dart';
@@ -13,32 +16,46 @@ class MeasurementCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final RedirectionToMeasurementsBloc _bloc =
+        Provider.of<RedirectionToMeasurementsBloc>(context);
+
     return Container(
       width: 250,
       margin: const EdgeInsets.symmetric(vertical: 10),
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 15),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              MeasurementCardRiver(river: measurement.riverName),
-              MeasurementCardValue(
-                isCritical: measurement.isCritical,
-                maxValue: measurement.maxWaterLevel,
-                actualValue: measurement.actualWaterLevel,
-              ),
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  MeasurementCardLocation(location: measurement.locationName),
-                  MeasurementCardTimestamp(timestamp: measurement.timestamp),
-                ],
-              )
-            ],
+      child: InkWell(
+        onTap: () {
+          _bloc.inputEventSink.add(
+            RedirectionToMeasurementsEvent(
+              context: context,
+              sensorId: measurement.sensorId,
+            ),
+          );
+        },
+        child: Card(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                MeasurementCardRiver(river: measurement.riverName),
+                MeasurementCardValue(
+                  isCritical: measurement.isCritical,
+                  maxValue: measurement.maxWaterLevel,
+                  actualValue: measurement.actualWaterLevel,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    MeasurementCardLocation(location: measurement.locationName),
+                    MeasurementCardTimestamp(timestamp: measurement.timestamp),
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
