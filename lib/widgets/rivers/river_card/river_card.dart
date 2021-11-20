742d6cbe042fc78ea1c55fb9878e1dcc04330f95
Khrystyna <khrystynavysotska@gmail.com>
2021-11-20 22:16:51 +0200

    Added onTap event to the card

--- lib/widgets/rivers/river_card/river_card.dart
+++ lib/widgets/rivers/river_card/river_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:river_water_level/controllers/locations_state_controller.dart';
 import 'package:river_water_level/models/river.dart';
 import 'package:river_water_level/widgets/rivers/river_card/river_card_image.dart';
 import 'package:river_water_level/widgets/rivers/river_card/river_card_info.dart';
@@ -11,18 +12,28 @@ class RiverCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-      child: Column(
-        children: [
-          RiverCardImage(imageUrl: river.imageUrl),
-          RiverCardName(name: river.name),
-          RiverCardInfo(
-            length: river.length,
-            basinArea: river.basinArea,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => LocationsStateController(riverId: river.id),
           ),
-        ],
+        );
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        child: Column(
+          children: [
+            RiverCardImage(imageUrl: river.imageUrl),
+            RiverCardName(name: river.name),
+            RiverCardInfo(
+              length: river.length,
+              basinArea: river.basinArea,
+            ),
+          ],
+        ),
       ),
     );
   }
